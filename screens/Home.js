bb443bd310702e176ddf8b354467da576f42eef2
malzak <malzovich@gmail.com>
2022-02-16 16:46:59 +0100

    styliser header avec data dynamique

--- screens/Home.js
+++ screens/Home.js
@@ -11,6 +11,7 @@ const Home = ({ navigation }) => {
       country: "Allemagne",
       totalImg: 3,
       img: "https://cdn.pixabay.com/photo/2017/12/17/08/44/girl-3023853_960_720.jpg",
+      favColor: "blueviolet",
     },
     {
       id: "2",
@@ -19,6 +20,7 @@ const Home = ({ navigation }) => {
       country: "France",
       totalImg: 5,
       img: "https://cdn.pixabay.com/photo/2018/04/27/03/50/portrait-3353699_960_720.jpg",
+      favColor: "firebrick",
     },
     {
       id: "3",
@@ -27,6 +29,7 @@ const Home = ({ navigation }) => {
       country: "Espagne",
       totalImg: 4,
       img: "https://cdn.pixabay.com/photo/2019/08/13/05/39/girl-4402542_960_720.jpg",
+      favColor: "olive",
     },
     {
       id: "4",
@@ -35,6 +38,7 @@ const Home = ({ navigation }) => {
       country: "Italie",
       totalImg: 5,
       img: "https://cdn.pixabay.com/photo/2017/03/24/18/59/face-2171923_960_720.jpg",
+      favColor: "orangered",
     },
   ];
 
@@ -45,7 +49,18 @@ const Home = ({ navigation }) => {
           { backgroundColor: pressed ? Colors.clicked : Colors.white },
           globalStyles.profileItem,
         ]}
-        onPress={() => navigation.navigate("Portfolio", item)}
+        // onPress={() => navigation.navigate("Portfolio", item)}
+        onPress={() =>
+          navigation.navigate({
+            routeName: "Portfolio",
+            params: {
+              name: item.name,
+              country: item.country,
+              totalImg: item.totalImg,
+              favColor: item.favColor,
+            },
+          })
+        }
       >
         <Text style={globalStyles.titleText}>{item.name}</Text>
         <Image source={{ uri: item.img }} style={globalStyles.profileImg} />
--- screens/Portfolio.js
+++ screens/Portfolio.js
@@ -1,6 +1,7 @@
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, Platform } from "react-native";
 import React from "react";
 import { globalStyles } from "../styles/AppStyles";
+import Colors from "../styles/Colors";
 
 const Logo = () => {
   return (
@@ -16,12 +17,32 @@ const Portfolio = ({ navigation }) => {
       <Text style={globalStyles.text}>{navigation.getParam("name")}</Text>
       <Text style={globalStyles.text}>{navigation.getParam("country")}</Text>
       <Text style={globalStyles.text}>{navigation.getParam("totalImg")}</Text>
+      <Text style={globalStyles.text}>{navigation.getParam("favColor")}</Text>
     </View>
   );
 };
 
-Portfolio.navigationOptions = {
-  headerTitle: () => <Logo />,
-};
+// Portfolio.navigationOptions = {
+//   headerTitle: "Profil",
+//   headerStyle: {
+//     backgroundColor: Platform.OS === "android" ? "blue" : "",
+//   },
+//   headerTintColor: Platform.OS === "android" ? Colors.white : "",
+//   headerTitleStyle: {
+//     fontStyle: "italic",
+//   },
+// };
 
+Portfolio.navigationOptions = (navigationData) => {
+  const name = navigationData.navigation.getParam("name");
+  const favColor = navigationData.navigation.getParam("favColor");
+
+  return {
+    headerTitle: `Profil de ${name}`,
+    headerStyle: {
+      backgroundColor: favColor,
+    },
+    headerTintColor: Colors.white,
+  };
+};
 export default Portfolio;
