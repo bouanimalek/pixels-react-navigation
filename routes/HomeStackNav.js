c090138b50659bc3cb8d908cbc4e6ecd2497ba1f
malzak <malzovich@gmail.com>
2022-02-16 13:39:14 +0100

    navigation options

--- routes/HomeStackNav.js
+++ routes/HomeStackNav.js
@@ -3,13 +3,26 @@ import { createAppContainer } from "react-navigation";
 import Home from "../screens/Home";
 import Portfolio from "../screens/Portfolio";
 import Photo from "../screens/Photo";
+import Colors from "../styles/Colors";
 
 const screens = {
   Home: {
     screen: Home,
+    navigationOptions: {
+      title: "Accueil",
+      headerStyle: {
+        backgroundColor: Colors.lightBrown,
+      },
+    },
   },
   Portfolio: {
     screen: Portfolio,
+    navigationOptions: {
+      title: "Profil",
+      headerStyle: {
+        backgroundColor: Colors.lightBrown,
+      },
+    },
   },
   Photo: {
     screen: Photo,
