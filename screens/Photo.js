d9ece7f9ca15457e1b50d649a4ffe9c3f63bcf16
malzak <malzovich@gmail.com>
2022-02-18 18:29:36 +0100

    photo component

--- screens/Photo.js
+++ screens/Photo.js
@@ -1,30 +1,42 @@
-import { StyleSheet, Text, View, Button } from "react-native";
-import React, { useEffect } from "react";
+import React from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { globalStyles } from "../styles/AppStyles";
 
 const Photo = ({ navigation }) => {
-  // useEffect(() => {
-  //   console.log("Photo est monté");
-  //   return () => {
-  //     console.log("Photo est démonté");
-  //   };
-  // }, []);
-  const handlePress = () => {
-    navigation.pop();
-  };
+  const url = navigation.getParam("url");
+  const title = navigation.getParam("title");
+  const photoDes = navigation.getParam("photoDesc");
   return (
-    <View style={styles.container}>
-      <Text>Photo</Text>
-      <Button title="Vers Home" onPress={handlePress} />
-    </View>
+    <ScrollView style={globalStyles.container}>
+      <Image style={styles.selectedImage} source={{ uri: url }} />
+      <View style={styles.photoDescription}>
+        <Text style={globalStyles.titleText}>{title}</Text>
+        <Text style={styles.textDescription}>{photoDes}</Text>
+        <Text style={styles.textDescription}>{photoDes}</Text>
+      </View>
+    </ScrollView>
   );
 };
-
+Photo.navigationOptions = ({ navigation }) => {
+  const photoTitle = navigation.getParam("title");
+  return {
+    headerTitle: photoTitle.toUpperCase(),
+  };
+};
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "lightgreen",
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  selectedImage: {
+    width: "100%",
+    height: 300,
+  },
+  photoDescription: {
+    padding: 15,
+    fontSize: 25,
+    fontFamily: "InriaSans_400Regular",
+  },
+  textDescription: {
+    fontFamily: "InriaSans_400Regular",
+    fontSize: 20,
+    padding: 9,
   },
 });
 
