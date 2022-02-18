94683f283022960e77ca55708eae78f567d5107c
malzak <malzovich@gmail.com>
2022-02-18 11:38:35 +0100

    styliser le drawer

--- routes/DrawerNav.js
+++ routes/DrawerNav.js
@@ -2,20 +2,35 @@ import { createDrawerNavigator } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
 import StackNav from "./HomeStackNav";
 import FaqStackNav from "./FaqStackNav";
+import Colors from "../styles/Colors";
 
 const RouteConfig = {
   Home: {
     screen: StackNav,
+    navigationOptions: {
+      drawerLabel: "Accueil",
+    },
   },
   Faq: {
     screen: FaqStackNav,
+    navigationOptions: {
+      drawerLabel: "FAQ",
+    },
   },
 };
 
 const DrawerNavigatorConfig = {
   hideStatusBar: true,
   drawerBackgroundColor: "grey",
-  drawerWidth: 80,
+  drawerWidth: 110,
+  drawerType: "slide",
+  contentOptions: {
+    activeTintColor: Colors.clicked,
+    inactiveTintColor: Colors.white,
+    labelStyle: {
+      fontSize: 19,
+    },
+  },
 };
 const MainNavigator = createDrawerNavigator(RouteConfig, DrawerNavigatorConfig);
 
