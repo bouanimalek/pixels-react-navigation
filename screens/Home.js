5d366e1648c1bda4cfc584656df946d525cfc034
malzak <malzovich@gmail.com>
2022-03-09 15:11:17 +0100

    adding Modal in Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,20 @@
-import { FlatList, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { FlatList, View, Modal, StyleSheet, Text } from "react-native";
 import { DrawerActions } from "react-navigation-drawer";
 import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { MaterialIcons } from "@expo/vector-icons";
 import PressableItems from "../components/PressableItems";
 import MaterialIconsHeader from "../components/MaterialIconsHeader";
 import { globalStyles } from "../styles/AppStyles";
-import NoData from "../components/NoData";
+import Colors from "../styles/Colors";
 
 const Home = ({ navigation }) => {
+  useEffect(() => {
+    navigation.setParams({ handleModal: handleSettingsModal });
+  }, [handleSettingsModal]);
+
+  const [modalVisible, setModalVisible] = useState(false);
   const selectedCategories = useSelector(
     (state) => state.users.selectedCategories
   );
@@ -20,23 +27,33 @@ const Home = ({ navigation }) => {
       />
     );
   };
-  if (selectedCategories.length === 0) {
-    //message
-    return <NoData>Pas d'utilisateur à afficher </NoData>;
-  } else {
-    return (
-      <View style={globalStyles.container}>
-        <FlatList
-          data={selectedCategories}
-          renderItem={renderProfiles}
-          keyExtractor={(item) => item.id}
-        />
-      </View>
-    );
-  }
+
+  const handleSettingsModal = () => {
+    setModalVisible(!modalVisible);
+  };
+  return (
+    <View style={globalStyles.container}>
+      <Modal visible={modalVisible} animationType="slide">
+        <View style={styles.modalBody}>
+          <MaterialIcons
+            name="close"
+            size={24}
+            style={styles.modalClose}
+            onPress={handleSettingsModal}
+          />
+        </View>
+      </Modal>
+      <FlatList
+        data={selectedCategories}
+        renderItem={renderProfiles}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
 };
 
 Home.navigationOptions = ({ navigation }) => {
+  const handleModal = navigation.getParam("handleModal");
   return {
     headerLeft: () => (
       <HeaderButtons HeaderButtonComponent={MaterialIconsHeader}>
@@ -50,7 +67,27 @@ Home.navigationOptions = ({ navigation }) => {
         />
       </HeaderButtons>
     ),
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={MaterialIconsHeader}>
+        <Item title="Réglage" iconName="settings" onPress={handleModal} />
+      </HeaderButtons>
+    ),
   };
 };
+
+const styles = StyleSheet.create({
+  modalBody: {
+    flex: 1,
+    backgroundColor: Colors.gwhite,
+    marginVertical: 60,
+    padding: 20,
+    alignSelf: "center",
+    borderRadius: 10,
+    width: "90%",
+  },
+  modalClose: {
+    alignSelf: "flex-end",
+  },
+});
 
 export default Home;
